4fb4313e4c8223f16335a9185f8ec0b531351887
Kim Nguyen <design@kimisgold.com>
2013-09-17 17:19:04 -0400

    Move wrapper inside check for user.

--- views/public/group-manage-nav.php
+++ views/public/group-manage-nav.php
@@ -1,17 +1,19 @@
-<nav id="section-nav" class="navigation vertical">
-    <?php 
-        if(current_user()) {
-            $navArray = array();
-            if(isset($group)) {
-                if(is_allowed($group, 'edit')) {
-                    $navArray['Edit Group'] = array('label'=>__('Edit'), 'uri' => record_url($group, 'edit'));
-                }
-                
-                if(is_allowed($group, 'manage')) {
-                    $navArray['Manage Group'] = array('label'=>__('My Group Settings'), 'uri' => record_url($group, 'manage'));
-                }
+<?php 
+    if(current_user()) {
+        $navArray = array();
+        if(isset($group)) {
+            if(is_allowed($group, 'edit')) {
+                $navArray['Edit Group'] = array('label'=>__('Edit'), 'uri' => record_url($group, 'edit'));
             }
-            echo nav($navArray, 'group_manage_nav');
+            
+            if(is_allowed($group, 'manage')) {
+                $navArray['Manage Group'] = array('label'=>__('My Group Settings'), 'uri' => record_url($group, 'manage'));
+            }
         }
-    ?>
-</nav>
+        if (count($navArray) > 0) {
+            echo '<nav class="group-manage navigation">';
+            echo nav($navArray, 'group_manage_nav');
+            echo '</nav>';
+        }
+    }
+?>
