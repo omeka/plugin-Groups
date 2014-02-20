0b7bc78a32da8109b394938a247ab794acbc7772
patrickmj <patrickmjchnm@gmail.com>
2014-02-20 14:56:02 -0500

    add filters

--- views/admin/group/browse.php
+++ views/admin/group/browse.php
@@ -10,6 +10,16 @@ echo head(array('title' => 'Groups'));
 
 <div id='primary'>
 <?php echo pagination_links(); ?>
+<ul class="quick-filter-wrapper">
+    <li><a href="#" tabindex="0"><?php echo __('Quick Filter'); ?></a>
+    <ul class="dropdown">
+        <li><span class="quick-filter-heading"><?php echo __('Quick Filter') ?></span></li>
+        <li><a href="<?php echo url('groups/browse'); ?>"><?php echo __('View All') ?></a></li>
+        <li><a href="<?php echo url('groups/browse', array('flagged' => 0)); ?>"><?php echo __('Flagged'); ?></a></li>
+        <li><a href="<?php echo url('groups/browse', array('featured' => 1)); ?>"><?php echo __('Featured'); ?></a></li>
+    </ul>
+    </li>
+</ul>
     <table>
         <thead>
         <tr><th>Group</th><th>Visibility</th></tr>
